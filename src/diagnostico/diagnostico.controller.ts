e14f34104314f2956e2ad8cb04898478b1d817c0
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-11-22 17:21:11 -0500

    add class-transformer package and implement CRUD operations in Diagnostico, Medico, and Paciente controllers

--- src/diagnostico/diagnostico.controller.ts
+++ src/diagnostico/diagnostico.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UseInterceptors } from '@nestjs/common';
 import { DiagnosticoService } from './diagnostico.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { DiagnosticoDto } from './diagnostico.dto/diagnostico.dto';
+import { plainToInstance } from 'class-transformer';
+import { DiagnosticoEntity } from './diagnostico.entity/diagnostico.entity';
 
 @Controller('diagnosticos')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class DiagnosticoController {
     constructor(private readonly diagnosticoService: DiagnosticoService) {}
+
+    @Get()
+    async findAll() {
+        return await this.diagnosticoService.findAll();
+    }
+
+    @Get(':diagnosticoId')
+    async findOne(@Param('diagnosticoId') diagnosticoId: string) {
+        return await this.diagnosticoService.findOne(diagnosticoId);
+    }
+
+    @Post()
+    async create(@Body() diagnosticoDto: DiagnosticoDto) {
+        const diagnostico = plainToInstance(DiagnosticoEntity, diagnosticoDto);
+        return await this.diagnosticoService.create(diagnostico);
+    }
+
+    @Delete(':diagnosticoId')
+    @HttpCode(204)
+    async delete(@Param('diagnosticoId') diagnosticoId: string) {
+        await this.diagnosticoService.delete(diagnosticoId);
+    }
 }
--- src/medico/medico.controller.ts
+++ src/medico/medico.controller.ts
@@ -1,9 +1,34 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UseInterceptors } from '@nestjs/common';
 import { MedicoService } from './medico.service';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
+import { MedicoDto } from './medico.dto/medico.dto';
+import { plainToInstance } from 'class-transformer';
+import { MedicoEntity } from './medico.entity/medico.entity';
 
 @Controller('medicos')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class MedicoController {
     constructor(private readonly medicoService: MedicoService) {}
+
+    @Get()
+    async findAll() {
+        return await this.medicoService.findAll();
+    }
+
+    @Get(':medicoId')
+    async findOne(@Param('medicoId') medicoId: string) {
+        return await this.medicoService.findOne(medicoId);
+    }
+
+    @Post()
+    async create(@Body() medicoDto: MedicoDto) {
+        const medico = plainToInstance(MedicoEntity, medicoDto);
+        return await this.medicoService.create(medico);
+    }
+
+    @Delete(':medicoId')
+    @HttpCode(204)
+    async delete(@Param('medicoId') medicoId: string) {
+        await this.medicoService.delete(medicoId);
+    }
 }
